641778342a253075a8c85f0612eb3267710e9c43
viat495-ai <viat495@gmail.com>
2026-03-18 13:35:19 -0600

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { Providers } from "@/context/Providers";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Restaurant Reservation System",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<Providers>{children}</Providers>
+				<Toaster />
+			</body>
+		</html>
+	);
+}
